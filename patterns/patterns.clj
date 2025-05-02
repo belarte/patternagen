735050e981ff10442099cadd99d3387d17d163a6
Bruno Belarte <bruno.belarte@gmail.com>
2025-05-02 14:28:20 +0200

    Add task taking input string and converting it to list of strokes

--- patterns/patterns.clj
+++ patterns/patterns.clj
@@ -1,6 +1,36 @@
-(ns patterns)
+(ns patterns
+  (:require [clojure.string :as str]))
 
 (def patterns [{:pattern "RllK"}
                {:pattern "RLKK"}])
 
+(def strokes {\R :right-accent
+              \L :left-accent
+              \l :left-ghost
+              \K :kick})
+
 (defn ls [] (prn patterns))
+
+(defn- prepare
+  "Transforms the `input` string into a list of patterns."
+  [input]
+  {:pre [(string? input)]}
+  (-> input
+      (str/replace #"\s" "")
+      (str/split #",")))
+
+(defn- pattern-to-strokes [pattern]
+  (map #(strokes %) pattern))
+
+(defn- patterns-to-strokes [patterns]
+  (->> (prepare patterns)
+       (map pattern-to-strokes)))
+
+(defn generate [& args]
+  (prn (patterns-to-strokes (first args))))
+
+(comment
+  (pattern-to-strokes "RllK")
+  (prepare "RllK, RLKK ")
+  (let [args "RllK,RllK,RLKK,RLKK"]
+    (patterns-to-strokes args)))
