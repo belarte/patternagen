de2849feebcb3053d5fb1c687ccf921527a149a9
Bruno Belarte <bruno.belarte@gmail.com>
2025-05-03 16:15:31 +0200

    generate correct lilypond output for a given pattern

--- patterns/patterns.clj
+++ patterns/patterns.clj
@@ -10,10 +10,12 @@
 (defn valid-patterns? [patterns]
   (every? valid-pattern? patterns))
 
-(def strokes {\R :right-accent
-              \L :left-accent
-              \l :left-ghost
-              \K :kick})
+(def strokes {\R "sn16^\"R\"^\">\""
+              \L "sn16^\"L\"^\">\""
+              \l (str "\\once \\override Stem.font-size = -3\n"
+                      "\\once \\override NoteHead.font-size = -3\n"
+                      "\\parenthesize sn16^\"l\"")
+              \K "bd16^\"K\""})
 
 (defn- prepare
   "Transforms the `input` string into a valid list of patterns."
@@ -24,21 +26,33 @@
       (str/replace #"\s" "")
       (str/split #",")))
 
-(defn- pattern-to-strokes [pattern]
-  (map #(strokes %) pattern))
+(defn- pattern-to-strokes
+  "Transform the input pattern into the corresponding strokes."
+  [pattern]
+  {:pre  [(valid-pattern? pattern)]
+   :post [(string? %)]}
+  (let [strokes (map #(strokes %) pattern)
+        header  (str "%-- " pattern)]
+    (->> (conj strokes header)
+         (str/join "\n"))))
 
 (defn- patterns-to-strokes [patterns]
   (->> (prepare patterns)
-       (map pattern-to-strokes)))
+       (map pattern-to-strokes)
+       (str/join "\n\n")))
 
 (defn ls [] (prn patterns))
 
 (defn generate [& args]
-  (prn (patterns-to-strokes (first args))))
+  (println (patterns-to-strokes (first args))))
 
 (comment
   (valid-patterns? ["RllK" "RLK"])
   (pattern-to-strokes "RllK")
   (prepare "RllK, RLKK ")
+  (let [input "RllK"]
+    (->> input
+         (pattern-to-strokes)
+         (spit "test.ly")))
   (let [args "RllK,RllK,RLKK,RLKK"]
     (patterns-to-strokes args)))
